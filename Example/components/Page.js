caa6d5e0db0ee9e3c3fa207aac1a29a6a273399d
aspsptyd <devadiroot@gmail.com>
2023-12-01 15:30:53 +0700

    Design OPA done

--- Example/components/Page.js
+++ Example/components/Page.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, SafeAreaView, Text, View } from 'react-native';
+import {SafeAreaView, View } from 'react-native';
 
 const Page = ({ children }) => (
   <View>
     <SafeAreaView>{children}</SafeAreaView>
   </View>
 );
-
-const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    backgroundColor: 'white',
-    paddingTop: 40,
-    paddingHorizontal: 10,
-    paddingBottom: 10,
-  },
-});
 
 export default Page;
